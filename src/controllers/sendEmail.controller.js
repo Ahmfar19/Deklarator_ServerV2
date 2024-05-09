df52ad89104e23ea0d1778777f65fcac83a13b5c
Ahmad Faroun <ahmad.faroun@sato-global.com>
2024-05-09 23:30:29 +0200

    Fix some minor issues with this branch

--- src/controllers/sendEmail.controller.js
+++ src/controllers/sendEmail.controller.js
@@ -6,30 +6,33 @@ const EMAIL = config.get('EMAIL');
 const EMAIL_PASS = config.get('EMAIL_PASS');
 
 const sendEmail = async (to, subject, text) => {
-    let transporter = nodemailer.createTransport({
-        host: EMAIL_HOST, // POP/IMAP Server
-        port: 465, // SMTP Port
-        secure: true, // Indicates if the connection should use SSL
-        auth: {
-            user: EMAIL, // Your email address
-            pass: EMAIL_PASS // Your password
-        }
-    });
+    return new Promise((resolve) => {
+        let transporter = nodemailer.createTransport({
+            host: EMAIL_HOST, // POP/IMAP Server
+            port: 465, // SMTP Port
+            secure: true, // Indicates if the connection should use SSL
+            auth: {
+                user: EMAIL, // Your email address
+                pass: EMAIL_PASS // Your password
+            }
+        });
 
-    let mailOptions = {
-        from: EMAIL,
-        to: to,
-        subject: subject,
-        text: text
-    };
+        let mailOptions = {
+            from: EMAIL,
+            to: to,
+            subject: subject,
+            text: text
+        };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-            console.log("Error sending email");
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                // console.log("Error sending email", error);
+                resolve(false);
+            } else {
+                // console.log('Email sent: ' + info.response);
+                resolve(true);
+            }
+        });
     });
 }
 
--- src/helpers/emailMessages.js
+++ src/helpers/emailMessages.js
@@ -2,13 +2,16 @@
 const mailMessags = {
     loginMessage: {
         title:"Dear {0}",
-        body: "Congratulations on successfully registering with Deklarator \n. Below are your account details:\n Username: {0} and password: {1}",
+        body: "Congratulations on successfully registering with Deklarator.\nBelow are your account details: \nUsername: {0} and password: {1}",
     },
     pinMessage :{
         title:"Your Verification PIN Code",
-        body:"You have requested a PIN code for verification purposes.\n Please find your unique PIN code below: pinCode :{0}"
+        body:"You have requested a PIN code for verification purposes.\nPlease find your unique PIN code below.\npinCode :{0}"
+    },
+    reminder :{
+        title:"title",
+        body:"This is a body"
     } 
-
 };
 
 module.exports = mailMessags;
