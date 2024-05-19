85280d82112fb3a55b25d13fe2ba12de8dfb977f
osamafaroun1 <osamafaroun7@gmail.com>
2024-05-19 12:58:48 +0300

    first

--- src/controllers/sendReminder.controller.js
+++ src/controllers/sendReminder.controller.js
@@ -1,72 +1,70 @@
 const Reminder = require('../models/reminder.model');
-const Message = require('../models/message.model');
-const MessageType = require('../models/message_type.model');
-const User = require('../models/user.model');
+// const Message = require('../models/message.model');
+// const MessageType = require('../models/message_type.model');
+// const User = require('../models/user.model');
 const { sendEmailHtml } = require('./sendEmail.controller');
-const { getNowDate_time } = require('../helpers/utils');
+// const { getNowDate_time } = require('../helpers/utils');
 const path = require('path');
 const fs = require('fs');
 const mailMessags = require('../helpers/emailMessages');
 
+function getHtmlTemplate(tamplateName) {
+    const htmlTemplatePath = path.resolve(`assets/tampletes/${tamplateName}.html`);
+    const htmlTemplate = fs.readFileSync(htmlTemplatePath);
+    return htmlTemplate;
+}
+
+const sendEmails = async (templateId, templateArray, title) => {
+    const emailsSent = [];
+
+    for (const item of templateArray) {
+        const htmlTemplate = getHtmlTemplate(item.tamplate_name);
+        const emailSent = await sendEmailHtml(item.company_email, title, htmlTemplate);
+        emailsSent.push(emailSent);
+    }
+
+    return emailsSent;
+};
+
 const checkForReminder = async () => {
     try {
         const data = await Reminder.getReminders();
-
         if (!data.length) return;
-
-        const admins = await User.getAdmins();
-        const messageName = await MessageType.getMessageTypeByName('danger');
 
         const { title } = mailMessags.reminder;
 
-        for (let i = 0; i < data.length; i++) {
-            const htmlTemplatePath = path.resolve(`assets/tampletes/${data[i].tamplate_name}.html`);
-            const htmlTemplate = fs.readFileSync(htmlTemplatePath);
+        const templates = {
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            5: []
+        };
 
-            const emailSent = await sendEmailHtml(data[i].company_email, title, htmlTemplate);
-
-            if (data[i].recurrent) {
-                await Reminder.updateReminder(data[i].remender_id);
-            } else {
-                await Reminder.deleteReminder(data[i].remender_id);
-            }
-
-            if (!emailSent) {
-                const title = mailMessags.message.title;
-                const body = mailMessags.message.body;
-
-                const nowDateTime = getNowDate_time();
-
-                const bodyString = JSON.stringify({
-                    key: body,
-                    params: {
-                        0: data[i].company_name,
-                        1: data[i].tamplate_name,
-                    },
-                });
-
-                for (const admin of admins) {
-                    const message = new Message({
-                        staff_id: admin.staff_id,
-                        message_typ_id: messageName[0].message_typ_id,
-                        title: title,
-                        body: bodyString,
-                        date_time: nowDateTime,
-                        seen: false,
-                    });
-
-                    await message.save();
-                }
-            }
+        for (const item of data) {
+            templates[item.tamplate_id].push(item);
         }
+
+        const promises = Object.keys(templates).map(async (templateId) => {
+            if (templates[templateId].length) {
+                return await sendEmails(templateId, templates[templateId], title);
+            }
+        });
+
+        const allEmailsSent = await Promise.all(promises);
+
+
+        console.log(allEmailsSent);
+     
+
     } catch (error) {
-        // console.log(error.message);
+        // Handle errors appropriately
+        console.error(error);
     }
 };
-
 const sendReminderEmail = () => {
     const intervalInMilliseconds = 6 * 60 * 60 * 1000;
-    setInterval(function() {
+    setInterval(function () {
         checkForReminder();
     }, intervalInMilliseconds);
 };
@@ -75,3 +73,5 @@ module.exports = {
     sendReminderEmail,
     checkForReminder,
 };
+
+
