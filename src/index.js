ead15ca52a58cf27624ee94d303a21ac9f4bc0d8
Ahmad Faroun <ahmad996cyc@gmail.com>
2024-08-19 10:46:06 +0200

    clean up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const dotenv = require('dotenv');
-const https = require('https');
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 if (NODE_ENV === 'development') dotenv.config();
@@ -7,18 +6,5 @@ if (NODE_ENV === 'development') dotenv.config();
 const config = require('config');
 const PORT = config.get('PORT');
 const app = require('./app');
-
-const pingWebsite = () => {
-    // const url = 'https://system.deklarator.se';
-    const url = 'https://system.administreramer.se/';
-    https.get(url, () => {
-    }).on('error', () => {
-        return;
-    });
-};
-
-// setInterval(() => {
-//     pingWebsite();
-// }, 4 * 60 * 1000)
 
 app.listen(4000, () => console.log(`Server is running in ${NODE_ENV} mode on port: ${PORT}`));
